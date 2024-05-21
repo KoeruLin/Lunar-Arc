719631e1a6a2ceb37da59ceceac1553b358bf1cb
KoeruLin <167535514+KoeruLin@users.noreply.github.com>
2024-05-21 08:43:52 -0400

    Add files via upload

---
+++ enemysoldier.py
@@ -0,0 +1,10 @@
+import pygame
+
+class EnemySoldier:
+    def __init__(self, health, defense):
+        self.health = health
+        self.atk_spd = 1
+        self.defense = defense
+        self.speed = 10
+        self.block_count = 1
+
